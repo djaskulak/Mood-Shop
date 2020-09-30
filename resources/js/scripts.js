91d02b5aff12e0ea886367d59acd796a633d8bfb
Danika Jaskulak-Gonzalez <danikaherminise@gmail.com>
2020-09-29 22:13:48 -0700

    added in the beginning of the cart

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -3,31 +3,50 @@ import data from './data.js'
 
 // the length of our data determines how many times this loop goes around
 for (let i=0; i<data.length; ++i) {
-    let newDiv = document.createElement('div');
-      newDiv.className = 'item'
-    // display the image
-    let img = document.createElement('img');
-    img.src = data[i].image
-    img.width = 300
-    img.height = 300
-    newDiv.appendChild(img)
-  
-    let desc = document.createElement('P')
-    desc.innerText =data[i].desc
-    newDiv.appendChild(desc)
-    let price = document.createElement('P')
-    price.innerText = data[i].price
-    newDiv.appendChild(price)
-  
-    let button = document.createElement('button')
-    button.id = data[i].name
-  
-    // creates a custom attribute called data-price.
-    // That will hold the price for each element in the button
-    button.dataset.price = data[i].price
-    button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
-    // put new div inside items container
-    itemsContainer.appendChild(newDiv)
-  }
+  let newDiv = document.createElement('div');
+    newDiv.className = 'item'
+  // display the image
+  let img = document.createElement('img');
+  img.src = data[i].image
+  img.width = 300
+  img.height = 300
+  newDiv.appendChild(img)
 
+  let desc = document.createElement('P')
+  desc.innerText =data[i].desc
+  newDiv.appendChild(desc)
+  let price = document.createElement('P')
+  price.innerText = data[i].price
+  newDiv.appendChild(price)
+
+  let button = document.createElement('button')
+  button.id = data[i].name
+
+  // creates a custom attribute called data-price.
+  // That will hold the price for each element in the button
+  button.dataset.price = data[i].price
+  button.innerHTML = "Add to Cart"
+  newDiv.appendChild(button)
+  // put new div inside items container
+  itemsContainer.appendChild(newDiv)
+}
+
+//start of the cart
+const cart = [];
+
+//creating an object 
+const obj = {};
+
+//function for the cart
+function addItem (name, price) {
+  cart.push(name);
+}
+
+//function that shows items in the cart
+function showItems () {
+  console.log(`You have ${cart.length} items in your cart`);
+  console.log(cart);
+}
+
+//calling the function so we can see our items
+showItems();
